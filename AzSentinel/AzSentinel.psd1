c3d6e1433f7c4583fd2cda547988a6631dd87ca9
Ben Gelens <ben@bgelens.nl>
2019-09-06 22:08:30 +0200

    did some cleanup
    remove Get-AuthToken and made it internal
    
    simplified New-AzSentinalAlert as it contained 50% duplicate code
    
    defined required modules in manifest

--- AzSentinel/AzSentinel.psd1
+++ AzSentinel/AzSentinel.psd1
@@ -8,121 +8,128 @@
 
 @{
 
-# Script module or binary module file associated with this manifest.
-RootModule = 'AzSentinel.psm1'
+    # Script module or binary module file associated with this manifest.
+    RootModule = 'AzSentinel.psm1'
 
-# Version number of this module.
-ModuleVersion = '0.1.0'
+    # Version number of this module.
+    ModuleVersion = '0.1.0'
 
-# Supported PSEditions
-# CompatiblePSEditions = @()
+    # Supported PSEditions
+    CompatiblePSEditions = 'Core', 'Desktop'
 
-# ID used to uniquely identify this module
-GUID = '82a6a020-d628-41d1-ac34-5e90df53b10d'
+    # ID used to uniquely identify this module
+    GUID = '82a6a020-d628-41d1-ac34-5e90df53b10d'
 
-# Author of this module
-Author = 'Pouyan Khabazi'
+    # Author of this module
+    Author = 'Pouyan Khabazi'
 
-# Company or vendor of this module
-CompanyName = 'Wortell'
+    # Company or vendor of this module
+    CompanyName = 'Wortell'
 
-# Copyright statement for this module
-Copyright = '(c) Wortell Enterprise Security BV. All rights reserved.'
+    # Copyright statement for this module
+    Copyright = '(c) Wortell Enterprise Security BV. All rights reserved.'
 
-# Description of the functionality provided by this module
- Description = 'PowerShell module for Azure Sentinel'
+    # Description of the functionality provided by this module
+    Description = 'PowerShell module for Azure Sentinel'
 
-# Minimum version of the PowerShell engine required by this module
-# PowerShellVersion = ''
+    # Minimum version of the PowerShell engine required by this module
+    PowerShellVersion = '6.2'
 
-# Name of the PowerShell host required by this module
-# PowerShellHostName = ''
+    # Name of the PowerShell host required by this module
+    # PowerShellHostName = ''
 
-# Minimum version of the PowerShell host required by this module
-# PowerShellHostVersion = ''
+    # Minimum version of the PowerShell host required by this module
+    # PowerShellHostVersion = ''
 
-# Minimum version of Microsoft .NET Framework required by this module. This prerequisite is valid for the PowerShell Desktop edition only.
-# DotNetFrameworkVersion = ''
+    # Minimum version of Microsoft .NET Framework required by this module. This prerequisite is valid for the PowerShell Desktop edition only.
+    # DotNetFrameworkVersion = ''
 
-# Minimum version of the common language runtime (CLR) required by this module. This prerequisite is valid for the PowerShell Desktop edition only.
-# CLRVersion = ''
+    # Minimum version of the common language runtime (CLR) required by this module. This prerequisite is valid for the PowerShell Desktop edition only.
+    # CLRVersion = ''
 
-# Processor architecture (None, X86, Amd64) required by this module
-# ProcessorArchitecture = ''
+    # Processor architecture (None, X86, Amd64) required by this module
+    # ProcessorArchitecture = ''
 
-# Modules that must be imported into the global environment prior to importing this module
-# RequiredModules = @()
+    # Modules that must be imported into the global environment prior to importing this module
+    RequiredModules = @(
+        @{
+            ModuleName = 'Az.Accounts'
+            ModuleVersion = '1.5.2'
+        }
+        @{
+            ModuleName = 'powershell-yaml'
+            ModuleVersion = '0.4.0'
+        }
+    )
 
-# Assemblies that must be loaded prior to importing this module
-# RequiredAssemblies = @()
+    # Assemblies that must be loaded prior to importing this module
+    # RequiredAssemblies = @()
 
-# Script files (.ps1) that are run in the caller's environment prior to importing this module.
-# ScriptsToProcess = @()
+    # Script files (.ps1) that are run in the caller's environment prior to importing this module.
+    # ScriptsToProcess = @()
 
-# Type files (.ps1xml) to be loaded when importing this module
-# TypesToProcess = @()
+    # Type files (.ps1xml) to be loaded when importing this module
+    # TypesToProcess = @()
 
-# Format files (.ps1xml) to be loaded when importing this module
-# FormatsToProcess = @()
+    # Format files (.ps1xml) to be loaded when importing this module
+    # FormatsToProcess = @()
 
-# Modules to import as nested modules of the module specified in RootModule/ModuleToProcess
-# NestedModules = @()
+    # Modules to import as nested modules of the module specified in RootModule/ModuleToProcess
+    # NestedModules = @()
 
-# Functions to export from this module, for best performance, do not use wildcards and do not delete the entry, use an empty array if there are no functions to export.
-FunctionsToExport = @(
-    'Get-AuthToken', 'New-AzSentinelAlertRule', 'precheck', 'Set-AzSentinel'
-)
+    # Functions to export from this module, for best performance, do not use wildcards and do not delete the entry, use an empty array if there are no functions to export.
+    FunctionsToExport = @(
+        'New-AzSentinelAlertRule', 'Set-AzSentinel'
+    )
 
-# Cmdlets to export from this module, for best performance, do not use wildcards and do not delete the entry, use an empty array if there are no cmdlets to export.
-CmdletsToExport = @()
+    # Cmdlets to export from this module, for best performance, do not use wildcards and do not delete the entry, use an empty array if there are no cmdlets to export.
+    CmdletsToExport = @()
 
-# Variables to export from this module
-# VariablesToExport = '*'
+    # Variables to export from this module
+    # VariablesToExport = '*'
 
-# Aliases to export from this module, for best performance, do not use wildcards and do not delete the entry, use an empty array if there are no aliases to export.
-AliasesToExport = @()
+    # Aliases to export from this module, for best performance, do not use wildcards and do not delete the entry, use an empty array if there are no aliases to export.
+    AliasesToExport = @()
 
-# DSC resources to export from this module
-# DscResourcesToExport = @()
+    # DSC resources to export from this module
+    # DscResourcesToExport = @()
 
-# List of all modules packaged with this module
-# ModuleList = @()
+    # List of all modules packaged with this module
+    # ModuleList = @()
 
-# List of all files packaged with this module
-# FileList = @()
+    # List of all files packaged with this module
+    # FileList = @()
 
-# Private data to pass to the module specified in RootModule/ModuleToProcess. This may also contain a PSData hashtable with additional module metadata used by PowerShell.
-PrivateData = @{
+    # Private data to pass to the module specified in RootModule/ModuleToProcess. This may also contain a PSData hashtable with additional module metadata used by PowerShell.
+    PrivateData = @{
+        PSData = @{
+            # Tags applied to this module. These help with module discovery in online galleries.
+            Tags = @(
+                'Azure Sentinel',
+                'Sentinel',
+                'Azure'
+            )
 
-    PSData = @{
+            # A URL to the license for this module.
+            LicenseUri = 'https://github.com/wortell/AZSentinel/blob/master/LICENSE'
 
-        # Tags applied to this module. These help with module discovery in online galleries.
-        Tags = @(
-            'Azure Sentinel',
-            'Sentinel',
-            'Azure'
-        )
+            # A URL to the main website for this project.
+            ProjectUri = 'https://github.com/wortell/AZSentinel'
 
-        # A URL to the license for this module.
-        # LicenseUri = ''
+            # A URL to an icon representing this module.
+            # IconUri = ''
 
-        # A URL to the main website for this project.
-        ProjectUri = 'https://github.com/wortell/AZSentinel'
+            # ReleaseNotes of this module
+            # ReleaseNotes = ''
 
-        # A URL to an icon representing this module.
-        # IconUri = ''
+        } # End of PSData hashtable
 
-        # ReleaseNotes of this module
-        # ReleaseNotes = ''
+    } # End of PrivateData hashtable
 
-    } # End of PSData hashtable
+    # HelpInfo URI of this module
+    # HelpInfoURI = ''
 
-} # End of PrivateData hashtable
-
-# HelpInfo URI of this module
-# HelpInfoURI = ''
-
-# Default prefix for commands exported from this module. Override the default prefix using Import-Module -Prefix.
-# DefaultCommandPrefix = ''
+    # Default prefix for commands exported from this module. Override the default prefix using Import-Module -Prefix.
+    # DefaultCommandPrefix = ''
 
 }
--- AzSentinel/Public/New-AzSentinelAlertRule.ps1
+++ AzSentinel/Public/New-AzSentinelAlertRule.ps1
@@ -1,8 +1,6 @@
 #requires -module @{ModuleName = 'Az.Accounts'; ModuleVersion = '1.5.2'}
 #requires -module @{ModuleNAme = 'powershell-yaml'; ModuleVersion = '0.4.0'}
-#requires -version 6.0
-
-using module Az.Accounts
+#requires -version 6.2
 
 function New-AzSentinelAlertRule {
     <#
@@ -27,224 +25,105 @@ function New-AzSentinelAlertRule {
     In this example you can select multiple JSON files and Pipeline it to the module
     #>
 
-    [cmdletbinding(SupportsShouldProcess = $true, ConfirmImpact = 'High')]
+    [CmdletBinding(SupportsShouldProcess, ConfirmImpact = 'High')]
     param (
-        # Parameter help description
         [Parameter(Mandatory)]
-        [string]$Subscription,
+        [ValidateNotNullOrEmpty()]
+        [string] $Subscription,
 
-        # Parameter help description
         [Parameter(Mandatory)]
-        [string]$ResourceGroup,
+        [ValidateNotNullOrEmpty()]
+        [string] $ResourceGroup,
 
-        # Parameter help description
         [Parameter(Mandatory)]
-        [string]$Workspace,
+        [ValidateNotNullOrEmpty()]
+        [string] $Workspace,
 
-        # Parameter help description
-        [Parameter(Mandatory,
-            ValueFromPipeline)]
-        [string]$SettingsFile
+        [Parameter(Mandatory, ValueFromPipeline)]
+        [ValidateScript( { (Test-Path -Path $_) -and ($_.Extension -in '.json', '.yaml', '.yml') })]
+        [System.IO.FileInfo] $SettingsFile
     )
 
     begin {
-        if (!$authHeader) {
-            $authHeader = Get-AuthToken
-        }
         precheck
     }
-
     process {
-        # Variables
         $errorResult = ''
 
         $getUri = "https://management.azure.com/subscriptions/$Subscription/resourceGroups/$ResourceGroup/providers/Microsoft.OperationalInsights/workspaces/$Workspace/providers/Microsoft.SecurityInsights/alertRules?api-version=2019-01-01-preview"
 
-        # If JSON Format
-        if (($SettingsFile.Split('.')[-1]) -eq 'json') {
+        if ($SettingsFile.Extension -eq '.json') {
             try {
-                if (Test-Path $SettingsFile) {
-                    $analytics = (Get-Content $SettingsFile -Raw | ConvertFrom-Json).analytics
-                    Write-Verbose "Found $($analytics.count) rules"
-                }
-                else {
-                    Write-Error "JSON file not found" -ErrorAction Stop
-                }
-            }
-            catch {
+                $analytics = (Get-Content $SettingsFile -Raw | ConvertFrom-Json -ErrorAction Stop).analytics
+                Write-Verbose -Message "Found $($analytics.count) rules"
+            } catch {
                 Write-Verbose $_
-                Write-Error "Unable to convert JSON file"
+                Write-Error -Message 'Unable to convert JSON file' -ErrorAction Stop
             }
-
-            foreach ($item in $analytics) {
-                Write-Verbose "Started with rule: $($item.displayName)"
-
-                $guid = (New-Guid).Guid
-
-                try {
-                    Write-Verbose "Getting all current Analytic rules"
-                    $contents = Invoke-webrequest -Uri $getUri -Method get -Headers $authHeader
-                    $content = ($contents.Content | ConvertFrom-Json).value | Where-Object { $_.properties.displayName -eq $item.displayName }
-
-                    if ($content) {
-                        Write-Verbose "Rule $($item.displayName) exists in Azure Sentinel"
-
-                        $item | Add-Member -NotePropertyName name -NotePropertyValue $content.name -Force
-                        $item | Add-Member -NotePropertyName etag -NotePropertyValue $content.etag -Force
-                        $item | Add-Member -NotePropertyName Id -NotePropertyValue $content.id -Force
-
-                        $uri = "https://management.azure.com/subscriptions/$Subscription/resourceGroups/$ResourceGroup/providers/Microsoft.OperationalInsights/workspaces/$Workspace/providers/Microsoft.SecurityInsights/alertRules/$($content.name)?api-version=2019-01-01-preview"
-                    }
-                    else {
-                        Write-Verbose "Rule $($item.displayName) doesn't exists in Azure Sentinel"
-
-                        $item | Add-Member -NotePropertyName name -NotePropertyValue $guid -Force
-                        $item | Add-Member -NotePropertyName etag -NotePropertyValue $null -Force
-                        $item | Add-Member -NotePropertyName Id -NotePropertyValue "/subscriptions/$Subscription/resourceGroups/$ResourceGroup/providers/Microsoft.OperationalInsights/workspaces/$Workspace/providers/Microsoft.SecurityInsights/alertRules/$guid" -Force
-
-                        $uri = "https://management.azure.com/subscriptions/$Subscription/resourceGroups/$ResourceGroup/providers/Microsoft.OperationalInsights/workspaces/$Workspace/providers/Microsoft.SecurityInsights/alertRules/$($guid)?api-version=2019-01-01-preview"
-                    }
-                }
-                catch {
-                    $errorReturn = $_
-                    $errorResult = ($errorReturn | ConvertFrom-Json ).error
-                    Write-Verbose $_
-                    Write-Error "Unable to connect to APi to get Analytic rules with message: $($errorResult.message)" -ErrorAction Stop
-                }
-
-                try {
-                    $bodyAlertProp = [alertProp]::new(
-                        $item.name,
-                        $item.displayName,
-                        $item.description,
-                        $item.severity,
-                        $item.enabled,
-                        $item.query,
-                        $item.queryFrequency,
-                        $item.queryPeriod,
-                        $item.triggerOperator,
-                        $item.triggerThreshold,
-                        $item.suppressionDuration,
-                        $item.suppressionEnabled,
-                        $item.tactics
-                    )
-                    $body = [AlertRule]::new( $item.name, $item.etag, $bodyAlertProp, $item.Id)
-                }
-                catch {
-                    Write-Error "Unable to initiate class with error: $($_.Exception.Message)" -ErrorAction Stop
-                }
-
-                Write-Output ($body.Properties | Format-List | Format-Table | Out-String)
-
-                if ($content) {
-                    $compareResult = Compare-Policy -ReferenceTemplate ($content.properties | Select-Object * -ExcludeProperty lastModifiedUtc, alertRuleTemplateName) -DifferenceTemplate ($body.Properties | Select-Object * -ExcludeProperty Name)
-                    if ($compareResult) {
-                        Write-Verbose "Found Differences for rule: $($item.displayName)"
-                        Write-Output ($compareResult | Format-Table | Out-String)
-
-                        if ($PSCmdlet.ShouldProcess("Do you want to update profile: $($body.Properties.DisplayName)")) {
-                            try {
-                                $result = Invoke-webrequest -Uri $uri -Method Put -Headers $authHeader -Body ($body | ConvertTo-Json)
-                                Write-Output $result.StatusDescription
-                            }
-                            catch {
-                                $errorReturn = $_
-                                $errorResult = ($errorReturn | ConvertFrom-Json ).error
-                                Write-Verbose $_.Exception.Message
-                                Write-Error "Unable to invoke webrequest with error message: $($errorResult.message)" -ErrorAction Stop
-                            }
-                        }
-                        else {
-                            Write-Output "No change have been made for rule $($item.displayName), deployment aborted"
-                        }
-                    }
-                }
-                else {
-                    Write-Verbose "Creating new rule: $($item.displayName)"
-
-                    try {
-                        $result = Invoke-webrequest -Uri $uri -Method Put -Headers $authHeader -Body ($body | ConvertTo-Json)
-                        Write-Output $result.StatusDescription
-                    }
-                    catch {
-                        $errorReturn = $_
-                        $errorResult = ($errorReturn | ConvertFrom-Json ).error
-                        Write-Verbose $_.Exception.Message
-                        Write-Error "Unable to invoke webrequest with error message: $($errorResult.message)" -ErrorAction Stop
-                    }
-                }
-            }
-
-        }
-        # End of JSON
-
-        # If YAML format
-        elseif (($SettingsFile.Split('.')[-1]) -eq 'yaml' -or ($SettingsFile.Split('.')[-1]) -eq 'yml') {
+        } elseif ($SettingsFile.Extension -in '.yaml', 'yml') {
             try {
-                if (Test-Path $SettingsFile) {
-                    $item = (Get-Content $SettingsFile -Raw | ConvertFrom-Yaml)
-                    Write-Verbose "Found compatibel file"
-                }
-                else {
-                    Write-Error "YAML file not found" -ErrorAction Stop
-                }
+                $analytics = [pscustomobject](Get-Content $SettingsFile -Raw | ConvertFrom-Yaml -ErrorAction Stop) | Add-Member -MemberType AliasProperty -Name DisplayName -Value name -PassThru
+                Write-Verbose -Message 'Found compatibel yaml file'
+            } catch {
+                Write-Verbose $_
+                Write-Error -Message 'Unable to convert yaml file' -ErrorAction Stop
             }
-            catch {
-                Write-Verbose $_.Exception.Message
-                Write-Error "Unable to convert YAML file" -ErrorAction Stop
-            }
+        }
 
-            Write-Verbose "Started with rule: $($item.name)"
+        foreach ($item in $analytics) {
+            Write-Verbose -Message "Started with rule: $($item.displayName)"
 
             $guid = (New-Guid).Guid
 
             try {
-                Write-Verbose "Getting all current Analytic rules"
-                $contents = Invoke-webrequest -Uri $getUri -Method get -Headers $authHeader
-                $content = ($contents.Content | ConvertFrom-Json).value | Where-Object { $_.properties.displayName -eq $item.name }
+                Write-Verbose -Message 'Getting all current Analytic rules'
+                $contents = Invoke-WebRequest -Uri $getUri -Method Get -Headers $script:authHeader -UseBasicParsing -ErrorAction Stop
+                $content = ($contents.Content | ConvertFrom-Json).value | Where-Object { $_.properties.displayName -eq $item.displayName }
 
                 if ($content) {
-                    Write-Verbose "Rule $($item.name) exists in Azure Sentinel"
+                    Write-Verbose -Message "Rule $($item.displayName) exists in Azure Sentinel"
 
-                    $item | Add-Member -NotePropertyName guid -NotePropertyValue $content.name -Force
+                    $item | Add-Member -NotePropertyName name -NotePropertyValue $content.name -Force
                     $item | Add-Member -NotePropertyName etag -NotePropertyValue $content.etag -Force
                     $item | Add-Member -NotePropertyName Id -NotePropertyValue $content.id -Force
 
                     $uri = "https://management.azure.com/subscriptions/$Subscription/resourceGroups/$ResourceGroup/providers/Microsoft.OperationalInsights/workspaces/$Workspace/providers/Microsoft.SecurityInsights/alertRules/$($content.name)?api-version=2019-01-01-preview"
-                }
-                else {
-                    Write-Verbose "Rule $($item.name) doesn't exists in Azure Sentinel"
+                } else {
+                    Write-Verbose -Message "Rule $($item.displayName) doesn't exists in Azure Sentinel"
 
-                    $item | Add-Member -NotePropertyName guid -NotePropertyValue $guid -Force
+                    $item | Add-Member -NotePropertyName name -NotePropertyValue $guid -Force
                     $item | Add-Member -NotePropertyName etag -NotePropertyValue $null -Force
                     $item | Add-Member -NotePropertyName Id -NotePropertyValue "/subscriptions/$Subscription/resourceGroups/$ResourceGroup/providers/Microsoft.OperationalInsights/workspaces/$Workspace/providers/Microsoft.SecurityInsights/alertRules/$guid" -Force
 
                     $uri = "https://management.azure.com/subscriptions/$Subscription/resourceGroups/$ResourceGroup/providers/Microsoft.OperationalInsights/workspaces/$Workspace/providers/Microsoft.SecurityInsights/alertRules/$($guid)?api-version=2019-01-01-preview"
                 }
-            }
-            catch {
+            } catch {
                 $errorReturn = $_
                 $errorResult = ($errorReturn | ConvertFrom-Json ).error
                 Write-Verbose $_
                 Write-Error "Unable to connect to APi to get Analytic rules with message: $($errorResult.message)" -ErrorAction Stop
             }
 
-            $bodyAlertProp = [alertProp]::new(
-                $item.guid,
-                $item.name,
-                $item.description,
-                $item.severity,
-                $item.enabled,
-                $item.query,
-                $item.queryFrequency,
-                $item.queryPeriod,
-                $item.triggerOperator,
-                $item.triggerThreshold,
-                $item.suppressionDuration,
-                $item.suppressionEnabled,
-                $item.tactics
-            )
-            $body = [AlertRule]::new( $item.guid, $item.etag, $bodyAlertProp, $item.Id )
+            try {
+                $bodyAlertProp = [AlertProp]::new(
+                    $item.name,
+                    $item.displayName,
+                    $item.description,
+                    $item.severity,
+                    $item.enabled,
+                    $item.query,
+                    $item.queryFrequency,
+                    $item.queryPeriod,
+                    $item.triggerOperator,
+                    $item.triggerThreshold,
+                    $item.suppressionDuration,
+                    $item.suppressionEnabled,
+                    $item.tactics
+                )
+                $body = [AlertRule]::new( $item.name, $item.etag, $bodyAlertProp, $item.Id)
+            } catch {
+                Write-Error "Unable to initiate class with error: $($_.Exception.Message)" -ErrorAction Stop
+            }
 
             Write-Output ($body.Properties | Format-List | Format-Table | Out-String)
 
@@ -256,44 +135,31 @@ function New-AzSentinelAlertRule {
 
                     if ($PSCmdlet.ShouldProcess("Do you want to update profile: $($body.Properties.DisplayName)")) {
                         try {
-                            $result = Invoke-webrequest -Uri $uri -Method Put -Headers $authHeader -Body ($body | ConvertTo-Json)
+                            $result = Invoke-webrequest -Uri $uri -Method Put -Headers $script:authHeader -Body ($body | ConvertTo-Json)
                             Write-Output $result.StatusDescription
-                        }
-                        catch {
+                        } catch {
                             $errorReturn = $_
                             $errorResult = ($errorReturn | ConvertFrom-Json ).error
                             Write-Verbose $_.Exception.Message
                             Write-Error "Unable to invoke webrequest with error message: $($errorResult.message)" -ErrorAction Stop
                         }
-                    }
-                    else {
+                    } else {
                         Write-Output "No change have been made for rule $($item.displayName), deployment aborted"
                     }
                 }
-            }
-            else {
+            } else {
                 Write-Verbose "Creating new rule: $($item.displayName)"
 
                 try {
-                    $result = Invoke-webrequest -Uri $uri -Method Put -Headers $authHeader -Body ($body | ConvertTo-Json)
+                    $result = Invoke-webrequest -Uri $uri -Method Put -Headers $script:authHeader -Body ($body | ConvertTo-Json)
                     Write-Output $result.StatusDescription
-                }
-                catch {
+                } catch {
                     $errorReturn = $_
                     $errorResult = ($errorReturn | ConvertFrom-Json ).error
                     Write-Verbose $_.Exception.Message
                     Write-Error "Unable to invoke webrequest with error message: $($errorResult.message)" -ErrorAction Stop
                 }
             }
-
         }
-        # End of YAML
-
-        # If no match found
-        else {
-            Write-Error "File extension $($SettingsFile.Split('.')[-1]) is not supported" -ErrorAction Stop
-        }
-        # End
-
     }
 }
