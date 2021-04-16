3f2ef2f68df631199a584d7ecb44b3a4ed5f464e
Pouyan Khabazi <pkhabazi@outlook.com>
2021-04-16 14:05:19 +0200

    new watchlist

--- AzSentinel/Classes/Watchlist.ps1
+++ AzSentinel/Classes/Watchlist.ps1
@@ -1,8 +1,31 @@
 class Watchlist {
 
     [string]$DisplayName
+    [string]$Description
+    [string]$rawContent
+    [int]$numberOfLinesToSkip
 
-    Watchlist (){
+    $properties
 
+    watchList ($DisplayName, $rawContent, $Description, $numberOfLinesToSkip) {
+        $this.properties = @{
+            contentType = "text/csv"
+            description = $Description
+            displayName = $DisplayName
+            numberOfLinesToSkip = if ($numberOfLinesToSkip) { $numberOfLinesToSkip } else { 0 }
+            provider = "Microsoft"
+            rawContent = $rawContent
+            "source" = "Local file"
+        }
     }
 }
+
+
+$body = [Watchlist]::new(
+    "displaynaam",
+    "tes1,test2",
+    "description",
+    ""
+)
+
+$body.properties
--- AzSentinel/Public/New-AzSentinelWatchlist.ps1
+++ AzSentinel/Public/New-AzSentinelWatchlist.ps1
@@ -11,10 +11,10 @@ function New-AzSentinelWatchlist {
     Enter the subscription ID, if no subscription ID is provided then current AZContext subscription will be used
     .PARAMETER WorkspaceName
     Enter the Workspace name
-    .PARAMETER DisplayName
+    .PARAMETER Name
     Enter the Display name for the hunting rule
     .EXAMPLE
-    New-AzSentinelHuntingRule -WorkspaceName "" -DisplayName "" -Description "" -Tactics "","" -Query ''
+    New-AzSentinelHuntingRule -WorkspaceName "" -Name "" -Description "" -Tactics "","" -Query ''
     In this example you create a new hunting rule by defining the rule properties from CMDLET
     #>
 
@@ -30,8 +30,16 @@ function New-AzSentinelWatchlist {
         [string] $WorkspaceName,
 
         [Parameter(Mandatory)]
-        [string] $DisplayName
+        [string] $Name,
 
+        [Parameter(Mandatory)]
+        [string] $Description,
+
+        [Parameter(Mandatory = $false)]
+        [System.IO.FileInfo] $WatchListFile,
+
+        [Parameter(Mandatory = $false)]
+        [psobject] $WatchListObject
     )
 
     begin {
@@ -52,17 +60,47 @@ function New-AzSentinelWatchlist {
             }
         }
 
-        $item = @{ }
+        if ($WatchListFile) {
+            if ($WatchListFile.Extension -eq '.csv') {
+                try {
+                    $watchlistInput = (Get-Content $WatchListFile -Raw)
+                }
+                catch {
+                    Write-Error $_.Exception.Message
+                }
+            } elseif ($WatchListFile.Extension -eq '.json') {
+                try {
+                    $watchlistInput = (Get-Content $WatchListFile | ConvertTo-Csv)
+                }
+                catch {
+                    Write-Error $_.Exception.Message
+                    return
+                }
+            }
+            else {
+                Write-Error "Not supported file extensions"
+            }
+        }
+        elseif ($null -ne $WatchListObject) {
+            $watchlistInput = $WatchListObject
+        }
+        else {
+            #empty example
+            $watchlistInput = "This line will be skipped\nheader1,header2\nvalue1,value2"
+        }
 
-        Write-Verbose -Message "Creating new Watchlists: $($DisplayName)"
+        Write-Verbose -Message "Creating new Watchlists: $($Name)"
 
         try {
-            Write-Verbose -Message "Get Watchlists rule $DisplayName"
-            $content = Get-AzSentinelWatchlist @arguments -DisplayName $DisplayName
+            Write-Verbose -Message "Get Watchlists rule $Name"
+            $item = Get-AzSentinelWatchlist @arguments -Name $Name -ErrorAction SilentlyContinue
 
-            if ($content) {
-                Write-Verbose -Message "Watchlists rule $($DisplayName) exists in Azure Sentinel"
-                $uri = "$script:baseUri/providers/Microsoft.SecurityInsights/watchlists/$($DisplayName)?api-version=2019-01-01-preview"
+            if ($item) {
+                Write-Verbose -Message "Watchlists rule $($Name) exists in Azure Sentinel"
+                $uri = "$script:baseUri/providers/Microsoft.SecurityInsights/watchlists/$($Name)?api-version=2019-01-01-preview"
+            }
+            else {
+                $uri = "$script:baseUri/providers/Microsoft.SecurityInsights/watchlists/$($Name)?api-version=2019-01-01-preview"
             }
         }
         catch {
@@ -74,28 +112,27 @@ function New-AzSentinelWatchlist {
             Build Class
         #>
         try {
-            # $bodyProp = [Hunting]::new(
-            #     $DisplayName,
-            #     $Query,
-            #     $Description,
-            #     $Tactics
-            # )
+            $body = [Watchlist]::new(
+                $Name,
+                $watchlistInput,
+                $Description,
+                0
+            )
 
-            # $body = [HuntingRule]::new( $item.name, $item.etag, $item.Id, $bodyProp)
-
-$body = @"
-{
-    "properties": {
-        "contentType": "text/csv",
-        "description": "csv1",
-        "displayName": "$DisplayName",
-        "numberOfLinesToSkip": "0",
-        "provider": "Microsoft",
-        "rawContent": "This line will be skipped\nheader1,header2\nvalue1,value2",
-        "source": "Local file"
-    }
-}
-"@
+             return $body.properties
+# $body = @"
+# {
+#     "properties": {
+#         "contentType": "text/csv",
+#         "description": "csv1",
+#         "Name": "$Name",
+#         "numberOfLinesToSkip": "0",
+#         "provider": "Microsoft",
+#         "rawContent": "$watchlistInput",
+#         "source": "Local file"
+#     }
+# }
+# "@
         }
         catch {
             Write-Error "Unable to initiate class with error: $($_.Exception.Message)" -ErrorAction Continue
@@ -104,15 +141,15 @@ $body = @"
             Try to create or update Hunting Rule
         #>
         try {
-            $result = Invoke-webrequest -Uri $uri -Method Put -Headers $script:authHeader -Body $body
+            $result = Invoke-RestMethod -Uri $uri -Method Put -Headers $script:authHeader -Body ($body | ConvertTo-Json)
             #$body.Properties | Add-Member -NotePropertyName status -NotePropertyValue $($result.StatusDescription) -Force
-            return $body.Properties
+            return $result.properties
 
-            Write-Verbose "Successfully updated hunting rule: $($item.displayName) with status: $($result.StatusDescription)"
+            Write-Verbose "Successfully updated watchlist rule: $($item.Name) with status: $($result.uploadStatus)"
         }
         catch {
             Write-Verbose $_
-            Write-Error "Unable to invoke webrequest for rule $($item.displayName) with error message: $($_.Exception.Message)" -ErrorAction Continue
+            Write-Error "Unable to invoke webrequest for rule $($item.Name) with error message: $($_.Exception.Message)" -ErrorAction Continue
 
         }
     }
