0a3a88e33158e6a791c42d659d35467b7a24b531
Pouyan Khabazi <pkhabazi@outlook.com>
2021-07-20 13:42:35 +0200

    update

--- AzSentinel/Public/New-AzSentinelWatchlist.ps1
+++ AzSentinel/Public/New-AzSentinelWatchlist.ps1
@@ -36,6 +36,9 @@ function New-AzSentinelWatchlist {
         [string] $Description,
 
         [Parameter(Mandatory = $false)]
+        [int] $NumberOfLinesToSkip = 0,
+
+        [Parameter(Mandatory = $false)]
         [System.IO.FileInfo] $WatchListFile,
 
         [Parameter(Mandatory = $false)]
@@ -67,10 +70,12 @@ function New-AzSentinelWatchlist {
                 }
                 catch {
                     Write-Error $_.Exception.Message
+                    return
                 }
-            } elseif ($WatchListFile.Extension -eq '.json') {
+            }
+            elseif ($WatchListFile.Extension -eq '.json') {
                 try {
-                    $watchlistInput = (Get-Content $WatchListFile | ConvertTo-Csv)
+                    $watchlistInput = (Get-Content $WatchListFile | ConvertTo-Csv )
                 }
                 catch {
                     Write-Error $_.Exception.Message
@@ -81,7 +86,7 @@ function New-AzSentinelWatchlist {
                 Write-Error "Not supported file extensions"
             }
         }
-        elseif ($null -ne $WatchListObject) {
+        elseif ($WatchListObject) {
             $watchlistInput = $WatchListObject
         }
         else {
@@ -89,68 +94,60 @@ function New-AzSentinelWatchlist {
             $watchlistInput = "This line will be skipped\nheader1,header2\nvalue1,value2"
         }
 
-        Write-Verbose -Message "Creating new Watchlists: $($Name)"
 
-        try {
-            Write-Verbose -Message "Get Watchlists rule $Name"
-            $item = Get-AzSentinelWatchlist @arguments -Name $Name -ErrorAction SilentlyContinue
+        if ($watchlistInput) {
+            Write-Verbose -Message "Creating new Watchlists: $($Name)"
 
-            if ($item) {
-                Write-Verbose -Message "Watchlists rule $($Name) exists in Azure Sentinel"
-                $uri = "$script:baseUri/providers/Microsoft.SecurityInsights/watchlists/$($Name)?api-version=2019-01-01-preview"
+            try {
+                Write-Verbose -Message "Get Watchlists rule $Name"
+                $item = Get-AzSentinelWatchlist @arguments -Name $Name -ErrorAction SilentlyContinue
+
+                if ($item) {
+                    Write-Verbose -Message "Watchlists rule $($Name) exists in Azure Sentinel"
+                    $uri = "$script:baseUri/providers/Microsoft.SecurityInsights/watchlists/$($Name)?api-version=2019-01-01-preview"
+                }
+                else {
+                    $uri = "$script:baseUri/providers/Microsoft.SecurityInsights/watchlists/$($Name)?api-version=2019-01-01-preview"
+                }
             }
-            else {
-                $uri = "$script:baseUri/providers/Microsoft.SecurityInsights/watchlists/$($Name)?api-version=2019-01-01-preview"
+            catch {
+                Write-Verbose $_
+                Write-Error "Unable to connect to APi to get Analytic rules with message: $($_.Exception.Message)" -ErrorAction Stop
+            }
+
+            <#
+                Build Class
+            #>
+            try {
+                $body = [Watchlist]::new(
+                    $Name,
+                    $watchlistInput,
+                    $Description,
+                    $NumberOfLinesToSkip
+                )
+            }
+            catch {
+                Write-Error "Unable to initiate class with error: $($_.Exception.Message)" -ErrorAction Continue
+            }
+
+            <#
+                Try to create or update Hunting Rule
+            #>
+            try {
+                $result = Invoke-RestMethod -Uri $uri -Method Put -Headers $script:authHeader -Body ($body | ConvertTo-Json)
+                #$body.Properties | Add-Member -NotePropertyName status -NotePropertyValue $($result.StatusDescription) -Force
+                return $result.properties
+
+                Write-Verbose "Successfully updated watchlist rule: $($item.Name) with status: $($result.uploadStatus)"
+            }
+            catch {
+                Write-Verbose $_
+                Write-Error "Unable to invoke webrequest for rule $($item.Name) with error message: $($_.Exception.Message)" -ErrorAction Continue
+
             }
         }
-        catch {
-            Write-Verbose $_
-            Write-Error "Unable to connect to APi to get Analytic rules with message: $($_.Exception.Message)" -ErrorAction Stop
-        }
-
-        <#
-            Build Class
-        #>
-        try {
-            $body = [Watchlist]::new(
-                $Name,
-                $watchlistInput,
-                $Description,
-                0
-            )
-
-             return $body.properties
-# $body = @"
-# {
-#     "properties": {
-#         "contentType": "text/csv",
-#         "description": "csv1",
-#         "Name": "$Name",
-#         "numberOfLinesToSkip": "0",
-#         "provider": "Microsoft",
-#         "rawContent": "$watchlistInput",
-#         "source": "Local file"
-#     }
-# }
-# "@
-        }
-        catch {
-            Write-Error "Unable to initiate class with error: $($_.Exception.Message)" -ErrorAction Continue
-        }
-        <#
-            Try to create or update Hunting Rule
-        #>
-        try {
-            $result = Invoke-RestMethod -Uri $uri -Method Put -Headers $script:authHeader -Body ($body | ConvertTo-Json)
-            #$body.Properties | Add-Member -NotePropertyName status -NotePropertyValue $($result.StatusDescription) -Force
-            return $result.properties
-
-            Write-Verbose "Successfully updated watchlist rule: $($item.Name) with status: $($result.uploadStatus)"
-        }
-        catch {
-            Write-Verbose $_
-            Write-Error "Unable to invoke webrequest for rule $($item.Name) with error message: $($_.Exception.Message)" -ErrorAction Continue
-
+        else {
+            Write-Error "Watchlist input is empty or false" -ErrorAction Stop
         }
     }
 }
